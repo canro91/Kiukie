c5b9ebf50946e51415473e783a8ed1ffc861ceca
canro91 <canro91@hotmail.com>
2020-01-20 10:21:29 -0500

    Add tests for stateful queue processor

---
+++ Kiukie.Tests/Unit/StatefulQueueProcessorTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Kiukie.Tests.Unit
+{
+    [TestFixture]
+    public class StatefulQueueProcessorTests
+    {
+        [Test]
+        public async Task ProcessAsync_EmptyQueue_ReturnsFalse()
+        {
+            var queue = new EmptyStatefulQueue();
+            var handler = new FakePayloadHandler();
+            var queueProcessor = new StatefulQueueProcessor<string>(queue, handler);
+
+            var processed = await queueProcessor.ProcessAsync();
+
+            Assert.IsFalse(processed);
+        }
+
+        [Test]
+        public async Task ProcessAsync_AnItemInQueue_ReturnsTrue()
+        {
+            var queue = new SingleItemStatefulQueue(new StringItem("An item"));
+            var handler = new FakePayloadHandler();
+            var queueProcessor = new StatefulQueueProcessor<string>(queue, handler);
+
+            var processed = await queueProcessor.ProcessAsync();
+
+            Assert.IsTrue(processed);
+        }
+
+        [Test]
+        public void ProcessAsync_ExceptionWhileProcessingItem_ThrowExceptionAndUpdatesItem()
+        {
+            var queue = new SingleItemStatefulQueue(new StringItem("An item"));
+            var handler = new ThrowExceptionPayloadHandler(new Exception("An exception"));
+            var queueProcessor = new StatefulQueueProcessor<string>(queue, handler);
+
+            Assert.ThrowsAsync<Exception>(() => queueProcessor.ProcessAsync());
+            Assert.IsTrue(queue.UpdateWasCalled);
+        }
+
+        [Test]
+        public async Task ProcessAsync_NoExceptionWhileProcessingItem_UpdatesItem()
+        {
+            var queue = new SingleItemStatefulQueue(new StringItem("An item"));
+            var handler = new FakePayloadHandler();
+            var queueProcessor = new StatefulQueueProcessor<string>(queue, handler);
+
+            await queueProcessor.ProcessAsync();
+
+            Assert.IsTrue(queue.UpdateWasCalled);
+        }
+    }
+
+    public class EmptyStatefulQueue : IStatefulQueue<string>
+    {
+        public Task<IQueueItem<string>> DequeueAsync()
+        {
+            return Task.FromResult((IQueueItem<string>)null);
+        }
+
+        public Task UpdateAsync(IQueueItem<string> item, Exception e = null)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class SingleItemStatefulQueue : IStatefulQueue<string>
+    {
+        public StringItem Item;
+        public bool UpdateWasCalled;
+
+        public SingleItemStatefulQueue(StringItem item)
+        {
+            Item = item;
+        }
+
+        public Task<IQueueItem<string>> DequeueAsync()
+        {
+            return Task.FromResult((IQueueItem<string>)Item);
+        }
+
+        public Task UpdateAsync(IQueueItem<string> item, Exception e = null)
+        {
+            UpdateWasCalled = true;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Kiukie/Implementations/StatefulQueueProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kiukie
+{
+    public class StatefulQueueProcessor<T> : IQueueProcessor
+    {
+        private readonly IStatefulQueue<T> Queue;
+        private readonly IPayloadHandler<T> PayloadHandler;
+
+        public StatefulQueueProcessor(IStatefulQueue<T> queue, IPayloadHandler<T> handler)
+        {
+            Queue = queue;
+            PayloadHandler = handler;
+        }
+
+        public async Task<bool> ProcessAsync()
+        {
+            var queueItem = await Queue.DequeueAsync();
+            if (queueItem == null)
+            {
+                return false;
+            }
+
+            Exception exception = null;
+            try
+            {
+                await PayloadHandler.ProcessAsync(queueItem.Payload);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+                throw;
+            }
+            finally
+            {
+                await Queue.UpdateAsync(queueItem, exception);
+            }
+            return true;
+        }
+    }
+}
